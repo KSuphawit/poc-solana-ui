d1d638c0853e05244504a847f1fcd8dca4627562
Suphawit Khruawittawat <suphawit.kh@dv.co.th>
2022-02-07 17:10:28 +0700

    deposit token

--- src/App.js
+++ src/App.js
@@ -22,19 +22,6 @@ const App = (props) => {
   const { PublicKey, SYSVAR_RENT_PUBKEY, SystemProgram } = web3;
   const cluster = props.cluster;
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    if (inputValue.length <= 0) {
-      alert("Please enter your amount.");
-      return;
-    }
-
-    setInputValue("");
-
-    await callRpcMintToken();
-  };
-
   const callRpcInitializeMint = async (e) => {
     e.preventDefault();
     try {
@@ -146,26 +133,98 @@ const App = (props) => {
         {
           accounts: {
             depositToken: depositToken,
-            // returnToken: returnToken,
+            returnToken: returnToken,
             programDepositTokenAssocTokenAcct:
               programDepositTokenAssocTokenAcct,
             userDepositTokenAssocTokenAcct: userDepositTokenAssocTokenAcct,
-            // userReturnTokenAssocTokenAcct: userReturnTokenAssocTokenAcct,
+            userReturnTokenAssocTokenAcct: userReturnTokenAssocTokenAcct,
             user: provider.wallet.publicKey,
             program: PROGRAM_ID,
             tokenProgram: TOKEN_PROGRAM_ID,
-            associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-            rent: SYSVAR_RENT_PUBKEY,
-            systemProgram: SystemProgram.programId,
+            // associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+            // rent: SYSVAR_RENT_PUBKEY,
+            // systemProgram: SystemProgram.programId,
           },
         }
       );
 
       console.log("Result callRpcDepositToken", depositTokenResult);
 
-      await callRpcMintToken();
+      // await callRpcMintToken();
     } catch (error) {
       console.log("Error callRpcDepositToken :", error);
+    }
+  };
+
+  const callRpcWithdrawToken = async (e) => {
+    e.preventDefault();
+    setInputValue("");
+    try {
+      const provider = getProvider(cluster);
+      const program = getProgram(cluster);
+      const withdrawToken = new PublicKey(
+        "CuxuCrT6FCAc5SUoGDoMVuf7UCLwAvzUmseq4a9VBNqw"
+      );
+      const [burningToken, _] = await PublicKey.findProgramAddress(
+        [new TextEncoder().encode("token")],
+        PROGRAM_ID
+      );
+
+      const programWithdrawTokenAssocTokenAcct =
+        await Token.getAssociatedTokenAddress(
+          ASSOCIATED_TOKEN_PROGRAM_ID,
+          TOKEN_PROGRAM_ID,
+          withdrawToken,
+          PROGRAM_ID
+        );
+
+      console.log(
+        ">>>>>>>>>>>>>>>>>>>>>>>>>>> programWithdrawTokenAssocTokenAcct ",
+        programWithdrawTokenAssocTokenAcct.toString()
+      );
+
+      const userWithdrawTokenAssocTokenAcct =
+        await Token.getAssociatedTokenAddress(
+          ASSOCIATED_TOKEN_PROGRAM_ID,
+          TOKEN_PROGRAM_ID,
+          withdrawToken,
+          provider.wallet.publicKey
+        );
+
+      console.log(
+        ">>>>>>>>>>>>>>>>>>>> userWithdrawTokenAssocTokenAcct ",
+        userWithdrawTokenAssocTokenAcct.toString()
+      );
+
+      const burningSource = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        burningToken,
+        provider.wallet.publicKey
+      );
+
+      const amount = +inputValue * Math.pow(10, 9);
+
+      const withdrawTokenResult = await program.rpc.withdrawToken(
+        new BN(amount),
+        {
+          accounts: {
+            withdrawToken: withdrawToken,
+            burningToken: burningToken,
+            programWithdrawTokenAssocTokenAcct:
+              programWithdrawTokenAssocTokenAcct,
+            userWithdrawTokenAssocTokenAcct: userWithdrawTokenAssocTokenAcct,
+            burningSource: burningSource,
+            user: provider.wallet.publicKey,
+            program: PROGRAM_ID,
+            tokenProgram: TOKEN_PROGRAM_ID,
+          },
+        }
+      );
+
+      console.log("Result callRpcWithdrawToken", withdrawTokenResult);
+    } catch (error) {
+      console.log("Error callRpcWithdrawToken :", error);
     }
   };
 
@@ -181,7 +240,7 @@ const App = (props) => {
           }}
         />
         <button type="submit" className="cta-button submit-gif-button">
-          Deposit Token
+          Submit
         </button>
       </form>
       {/*<form onSubmit={callRpcInitializeMint}>*/}
